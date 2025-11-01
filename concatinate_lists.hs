34b53f5a26186610a4120f175f9c34c27f8b831d
Sahaswari Senanayaka <smsahaswari@gmail.com>
2025-11-01 12:29:32 +0530

    Add second concatenation function and update main for testing

--- concatinate_lists.hs
+++ concatinate_lists.hs
@@ -4,8 +4,13 @@ concat :: [[a]] -> [a]
 concat [] = []
 concat (xs:xss) = xs ++ concat xss
 
+concat1 :: [[a]] -> [a]
+concat1 [] = []
+concat1 (xs:xss) = xs ++ concat1 xss
+
 main :: IO ()
 main = do
     let list = [[1,2,3], [4,5], [6]]
-    putStrLn $ "Concatenated list: " ++ show (concat list)
-
+    let list1 = [[7],[8]]
+    putStrLn $ "Concatenated list: " ++ show (concat list) 
+    putStrLn $ "Concatenated list: " ++ show (concat1 list1) -- Should print [7,8]
